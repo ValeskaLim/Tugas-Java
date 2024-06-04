1a928f9d7c0e91ac0489e15ec8e7d6a7d79778dd
Vinsensius Odilo <vinsen.sutedja@gmail.com>
2024-06-04 17:54:07 +0700

    vehicle dan parkinglot

--- after/src/Vehicle.java
+++ after/src/Vehicle.java
@@ -1,37 +1,35 @@
 public class Vehicle {
-    VehicleType vehicleType;
-    String registrationNumber;
-    Color color;
+    private VehicleType vehicleType;
+    private String registrationNumber;
+    private Color color;
 
     public Vehicle(VehicleType vehicleType, String registrationNumber, Color color) {
         this.vehicleType = vehicleType;
         this.registrationNumber = registrationNumber;
         this.color = color;
     }
-
+    
+// getter setter vehicle type
     public VehicleType getVehicleType() {
         return vehicleType;
     }
-
     public void setVehicleType(VehicleType vehicleType) {
         this.vehicleType = vehicleType;
     }
-
+    
+// getter setter registration number
     public String getRegistrationNumber() {
         return registrationNumber;
     }
-
     public void setRegistrationNumber(String registrationNumber) {
         this.registrationNumber = registrationNumber;
     }
-
+    
+// getter setter vehicle color
     public Color getColor() {
         return color;
     }
-
     public void setColor(Color color) {
         this.color = color;
     }
-
-
 }
